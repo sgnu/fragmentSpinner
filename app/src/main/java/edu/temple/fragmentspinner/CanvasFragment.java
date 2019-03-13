a1a8507d4c596e6afd5031debc34ca884e9c48c4
Sam <sam.nguyen.1223@gmail.com>
2019-03-13 06:10:15 -0400

    Added canvas fragment

---
+++ app/src/main/java/edu/temple/fragmentspinner/CanvasFragment.java
@@ -0,0 +1,29 @@
+package edu.temple.fragmentspinner;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CanvasFragment extends Fragment {
+
+
+    public CanvasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_canvas, container, false);
+    }
+
+}
